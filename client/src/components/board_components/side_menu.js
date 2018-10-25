c1407fb03b47823fdbe8de0c367507d4e288321c
LiamAndrewBriggs <liamandrewbriggs@gmail.com>
2018-10-25 16:41:26 +0100

    Autocomplete works
    Starting building a new task

--- client/src/components/board_components/side_menu.js
+++ client/src/components/board_components/side_menu.js
@@ -1,24 +1,90 @@
 import React, { Component } from 'react';
 import { View } from 'react-native'
-
+import Select from 'react-select';
 import queryString from 'query-string'
 
 import '../../styles/sideMenu.css';
 
+// const options = [
+//     { value: 'chocolate', label: 'Chocolate' },
+//     { value: 'strawberry', label: 'Strawberry' },
+//     { value: 'vanilla', label: 'Vanilla' }
+//   ];
+
 class sideMenu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedOption: null
+        };
+
+    }
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => {
+
+                if (res.loggedIn === "No User") {
+                    this.props.history.push("/user/login");
+                }
+
+                console.log(res)
+
+                var options = [];
+
+                res.user.forEach(e => {
+                    options.push({value: e._id, label: e.name })
+                });
+                
+
+                this.setState({
+                    user: res.loggedIn,
+                    options: options,
+                    selectedOption: null
+                })
+
+            })
+            .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+
+        var path = "http://localhost:5000/user";
+        const response = await fetch(path, {
+            credentials: "include"
+        });
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
+
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+    }
 
     createTask = async (e) => {
 
         e.preventDefault();
+        //DO FOR MULTIPLE USERS
+        var body = {
+            // "name": this.refs.taskName.value,
+            // "description": this.refs.description.value,
+            // "storyPoints": this.refs.points.value,
+            "stage": "toDo",
+            "assignedUsers": {
+                _userID : this.state.selectedOption[0].value,
+                userName: this.state.selectedOption[0].label
+            },
+        }
 
-        // var body = {
-        //     "name": this.refs.name.value,
-        //     "description": this.refs.description.value,
-        //     "startDate": this.refs.startDate.value,
-        //     "endDate": this.refs.endDate.value,
-        // }
-
-        console.log(this.refs);
+        console.log(this.state)
+        console.log(body);
 
         // const options = {
         //     method: 'POST',
@@ -47,7 +113,7 @@ class sideMenu extends Component {
 
         let drawerClass = 'side-menu';
         const query = queryString.parse(this.props.content)
-
+        const selectedOption  = this.state.selectedOption;
 
         if (this.props.show) {
             drawerClass = 'side-menu open';
@@ -69,7 +135,7 @@ class sideMenu extends Component {
                             <p id="loginp"> Task Name </p>
                             <input className="inputfield" ref="taskName" type="text" placeholder="Task name" />
                             <p id="loginp"> Description </p>
-                            <textarea className="inputfield" ref="desciption" type="text" placeholder="Description" rows="8" cols="45"/>
+                            <textarea className="inputfield" ref="desciption" type="text" placeholder="Description" rows="8" cols="45" />
                             <p id="loginp"> Story Points </p>
                             <select className="inputfield" ref="points" name="points">
                                 <option value="16">16</option>
@@ -79,9 +145,14 @@ class sideMenu extends Component {
                                 <option value="1">1</option>
                             </select>
                             <p id="loginp"> User </p>
-                            <input className="inputfield" ref="user" type="text" placeholder="" />
-                            <p id="loginp"> Support User  </p>
-                            <input className="inputfield" ref="supportuser" type="text" placeholder="" />
+                            <div id="selectBox">
+                                <Select
+                                    value={selectedOption}
+                                    isMulti
+                                    onChange={this.handleChange}
+                                    options={this.state.options}
+                                />
+                            </div>
                             <br />
                             {/* <p id="errortext">{this.state.error}</p> */}
                             <input id="loginbutton" className="btn btn-primary" type="submit" value="Log In" />
